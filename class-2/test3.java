31e2a02eff76237740a88d09d68d1d481ed8c76e
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-21 11:23:42 +0530

    sf

---
+++ class-2/test3.java
@@ -0,0 +1,10 @@
+class test3{
+    public void m1() {
+        System.out.println("Test Class m1 method");
+    }
+
+    public static void main(String[] args) {
+        test3 obj = new test3(); 
+        obj.m1();                
+   }
+}
---
+++ class-2/test4.java
@@ -0,0 +1,13 @@
+class test4 {
+    int A = 10; 
+
+    
+    void B () {
+        System.out.println("The number is: " + A);
+    }
+
+    public static void main(String[] args) {
+        test4 obj = new test4();   
+        obj.B();      
+    }
+}
