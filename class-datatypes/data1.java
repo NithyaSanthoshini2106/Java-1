f8dd29138799ea18a184d0085689aa61e62a82da
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-22 16:18:26 +0530

    dt

---
+++ class-datatypes/data1.java
@@ -0,0 +1,13 @@
+public class data1{
+
+
+    public static void main(String[] args){
+
+        byte b = 50;
+
+        int i = 2147483647;
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
