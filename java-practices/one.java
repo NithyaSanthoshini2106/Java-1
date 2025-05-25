454e69240b66fd2c7a996466f812b77718d18085
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-25 20:17:45 +0530

    okay

---
+++ java-practices/one.java
@@ -0,0 +1,17 @@
+public class one {
+    public static void main(String[] args) {
+       
+        int age = 25;                 
+        double height = 5.9;         
+        char grade = 'A';         
+        boolean isStudent = true;     
+        String name = "Nithya";        
+
+        // Display the values
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
