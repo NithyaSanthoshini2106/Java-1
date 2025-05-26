144084185e2b74d376153d003e0a89aa72cb3791
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-26 16:36:47 +0530

    ok

---
+++ java-practices/five.java
@@ -0,0 +1,20 @@
+public class five{
+
+     int a;
+     static int b;
+
+
+    public static void main(String[] args) {
+       int c=100;
+
+        
+        System.out.println(c);
+
+
+        
+        
+    }
+
+    
+}
+
---
+++ java-practices/four.java
@@ -0,0 +1,20 @@
+public class four{
+
+     int a;
+     static int b;
+
+
+    public static void main(String[] args) {
+       
+
+        
+        System.out.println(b);
+
+
+        
+        
+    }
+
+    
+}
+
