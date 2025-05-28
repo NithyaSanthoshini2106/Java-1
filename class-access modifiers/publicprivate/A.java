25d990bfbe98c109864b73342ccb262078d1780c
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-28 13:29:53 +0530

    am

---
+++ class-access modifiers/publicprivate/A.java
@@ -0,0 +1,17 @@
+package pack1;
+public class A{
+
+   public void m1(){
+        System.out.println(" class A - m1 method m1");
+    }
+    void m2(){
+           System.out.println("class A- defaut Method m2");
+    }
+    private void m3(){
+                System.out.println("class A- private Method m3");
+
+
+    }
+    
+}
+
