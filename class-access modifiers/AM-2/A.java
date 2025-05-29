49aa7408a39e80aa038514955b273a87c997097c
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-05-29 13:31:31 +0530

    ok

---
+++ class-access modifiers/AM-2/A.java
@@ -0,0 +1,19 @@
+package pack1;
+
+public class A {
+    public void m1(){
+        System.out.println(" class A - public m1 method");
+
+    }
+    void m2(){
+        System.out.println("class A - defalt m2 method");
+    }
+    private void m3(){
+        System.out.println("class A - private m3 method");
+
+    }
+    protected void m4(){
+        System.out.println("class A - protected m4 method");
+
+    }
+}
---
+++ class-access modifiers/AM-2/C.java
@@ -0,0 +1,10 @@
+package pack2;
+import pack1.A;
+
+public class C extends A {
+    public static void main(String[] args) {
+        C c=new C();
+        
+    }
+    
+}
