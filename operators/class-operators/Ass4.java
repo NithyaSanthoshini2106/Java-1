84ca39fda0342dc90cb3ec1a6052018deb63e381
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-06-04 13:39:40 +0530

    oper

---
+++ operators/class-operators/Ass4.java
@@ -0,0 +1,16 @@
+public class Ass4 {
+    public static void main(String[] args) {
+        int a = 5;
+        
+        System.out.println("c = 5: " + a) ;
+        a += 5;
+        System.out.println("c += 5: " + a); 
+
+        a -= 5;
+        System.out.println("c -= 5: " + a);
+        a *= 5;
+        System.out.println(" c *= 5:" +a);
+        
+        
+    }
+}
---
+++ operators/class-operators/Ass5.java
@@ -0,0 +1,22 @@
+
+public class Ass5 {
+    public static void main(String[] args) {
+        int a = 5;
+        
+        System.out.println("c = 5: " + a) ;
+        a += 5;
+        System.out.println("c += 5: " + a); 
+
+        a -= 5;
+        System.out.println("c -= 5: " + a);
+        a *= 5;
+        System.out.println(" c *= 5:" +a);
+
+        a /= 5;
+        System.out.println("c /= 5:" + a);
+
+        
+        
+        
+    }
+}
