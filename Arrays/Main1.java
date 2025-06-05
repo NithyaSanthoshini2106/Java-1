ccac9c1ac63b4c4593ecb20d3d4f395fca165cab
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-06-05 12:52:11 +0530

    txt

---
+++ Arrays/Main1.java
@@ -0,0 +1,15 @@
+public class Main1 {
+    public static void main(String[] args) {
+        String[] enames;
+        enames=new String[4];
+
+        System.err.println(enames);
+        System.out.println(enames[0]);
+        System.out.println(enames[1]);
+        System.out.println(enames[2]);
+        System.out.println(enames[3]);
+
+        
+    }
+    
+}
