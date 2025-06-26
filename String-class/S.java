3f0c89ee421c269e385c6368fec8478099973167
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-06-26 11:24:08 +0530

    String

---
+++ String-class/S.java
@@ -0,0 +1,7 @@
+public class S {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s = s.concat(" World"); 
+        System.out.println(s);  
+    }
+}
---
+++ String-class/Sbu2.java
@@ -0,0 +1,7 @@
+public class Sbu2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello Nithya Raju ");
+        sb.append(" kiran raju");
+        System.out.println(sb);  
+    }
+}
