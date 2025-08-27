2bd9715900ee11c8cb5f2f0b634704fee2cd48ab
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-08-27 07:17:24 +0530

    ok

---
+++ JAVA practice one/nine.java
@@ -0,0 +1,13 @@
+
+
+
+public class nine {
+    public static void main(String[] args) {
+        int num = 15;
+
+        if (num % 2 == 0)
+            System.out.println(num);
+        else
+            System.out.println(num);
+    }
+}
