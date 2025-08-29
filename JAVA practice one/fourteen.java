f481d44862d368738c26528887a9d560c3e23d71
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-08-29 22:51:47 +0530

    ok

---
+++ JAVA practice one/fourteen.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+public class fourteen{
+   public fourteen() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 98;
+      byte var2 = 98;
+      if (var1 > var2) {
+         System.out.println(var1);
+      } else {
+         System.out.println(var2);
+      }
+
+   }
+}
---
+++ JAVA practice one/thirteen.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class thirteen{
+   public thirteen() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 85;
+      byte var2 = 72;
+      if (var1 > var2) {
+         System.out.println(var1);
+      } else {
+         System.out.println(var2);
+      }
+
+   }
+}
