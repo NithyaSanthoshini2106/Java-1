eca86fca0160a2bb6c06e86d150756df16f3a1b2
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-08-31 06:41:09 +0530

    ok

---
+++ JAVA practice one/fiveteen.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+public class fiveteen{
+   public fiveteen() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 56;
+      byte var2 = 7;
+      if (var1 > var2) {
+         System.out.println(var1);
+      } else {
+         System.out.println(var2);
+      }
+
+   }
+}
