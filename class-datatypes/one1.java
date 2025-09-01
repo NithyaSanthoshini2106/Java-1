f11a9fcce728f0e467abe4fb19018bcd6e213f6d
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-01 10:55:17 +0530

    txt

---
+++ class-datatypes/one1.java
@@ -0,0 +1,25 @@
+
+
+
+
+public class one1 {
+    public static void main(String[] args) {
+        byte b = 100;             // byte
+        short s = 20000;          // short
+        int i = 100000;           // int
+        long l = 1000000000L;     // long
+        float f = 3.14f;          // float
+        double d = 3.14159;       // double
+        char c = 'A';             // char
+        boolean flag = true;      // boolean
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+}
---
+++ class-datatypes/two2.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class two2{
+    public static void main(String[] args) {
+        String name = "Nithya Spoorthi Raju";   // String
+        int[] numbers = {1, 2, 3, 4, 5};  // Array
+
+        System.out.println(name);
+        System.out.println(numbers[0]);
+    }
+}
