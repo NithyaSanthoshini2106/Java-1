1ccf588eea30d0018561dd766bdcbc7212490b7c
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-01 10:15:24 +0530

    ok

---
+++ JAVA practice one/seventeen.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+public class seventeen {
+    public static void main(String[] args) {
+        int num = 15345;
+
+        if (num % 3 == 0)
+            System.out.println(num);
+        else
+            System.out.println(num);
+    }
+}
---
+++ JAVA practice one/sixteen.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+public class sixteen{
+   public sixteen() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 05;
+      byte var2 = 90;
+      if (var1 > var2) {
+         System.out.println(var1);
+      } else {
+         System.out.println(var2);
+      }
+
+   }
+}
