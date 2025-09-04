3fe4e518d03e720d9737c716904d67ce634d08c0
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-04 14:48:59 +0530

    ok

---
+++ class-variables/five5.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class five5{
+
+    int a=1000;
+    int b=20087;
+    public static void main(String[] args) {
+        five5 e1=new five5();
+        System.out.println(e1);
+
+    
+    }
+}
---
+++ class-variables/six6.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+public class six6{
+
+    int a=13456700;
+    int b=87654300;
+    public static void main(String[] args) {
+        //System.out.println(a);
+        six6 e1=new six6();
+        System.out.println(e1);
+
+    
+    }
+}
