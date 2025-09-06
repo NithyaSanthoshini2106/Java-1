71a61771fc1275d55c3723e558f135ed221d632d
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-06 10:04:52 +0530

    ok

---
+++ class-datatypes/nine9.java
@@ -0,0 +1,31 @@
+
+
+
+
+class nine9 {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        byte b = 100;                // 1 byte
+        short s = 2000;              // 2 bytes
+        int i = 50000;               // 4 bytes
+        long l = 15000000000L;       // 8 bytes
+        float f = 5.75f;             // 4 bytes
+        double d = 19.99;            // 8 bytes
+        char c = 'A';                // 2 bytes
+        boolean bool = true;         // 1 bit
+
+        // Non-Primitive Data Type
+        String str = "Hello Java";
+
+        // Printing all
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+}
---
+++ class-datatypes/ten10.java
@@ -0,0 +1,9 @@
+
+
+class ten10 {
+    public static void main(String[] args) {
+        byte a = 10, b = 20;
+        byte sum = (byte)(a + b);
+        System.out.println( sum);
+    }
+}
