e3a53f55110e552fd6a7ad5776ee05bc95b418d7
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-08 09:31:33 +0530

    ok

---
+++ JAVA practice one/twe2.java
@@ -0,0 +1,12 @@
+
+
+
+import java.util.*;
+class twe2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt(), f = 1;
+    for (int i = 1; i <= n; i++) f *= i;
+    System.out.println(f);
+  }
+}
