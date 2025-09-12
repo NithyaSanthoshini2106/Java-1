9a864d0fb4cae8da8698db8a2c71c9566d6a3a7a
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-12 10:05:12 +0530

    ok

---
+++ JAVA practice one/twe9.java
@@ -0,0 +1,13 @@
+
+
+
+public class twe9 {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 == 0){
+            System.out.println(num);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
