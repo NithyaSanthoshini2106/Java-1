82956e59a73fc63438289d70f50bf25c338a89d8
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-14 10:43:00 +0530

    ok

---
+++ JAVA practice one/twe14.java
@@ -0,0 +1,10 @@
+
+
+
+public class twe14 {
+    public static void main(String[] args) {
+        int a = 1054, b = 20567;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
---
+++ JAVA practice one/twe15.java
@@ -0,0 +1,13 @@
+
+
+
+public class twe15 {
+    public static void main(String[] args) {
+        int num = 73245667;
+        if(num % 2 == 0)
+            System.out.println(num);
+        else
+            System.out.println(num);
+    }
+}
+
