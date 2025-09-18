cc8092318e99d4d2d986f4ea482af2411f67ea37
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-18 20:03:55 +0530

    ok

--- JAVA practice one/twe20.java
+++ JAVA practice one/twe20.java
@@ -1,6 +1,6 @@
 public class twe20 {
     public static void main(String[] args) {
-        for(int n=287655; n<=100; n++){
+        for(int n=287655; n<=108760; n++){
             boolean prime = true;
             for(int i=2; i<=n/2; i++){
                 if(n % i == 0){
