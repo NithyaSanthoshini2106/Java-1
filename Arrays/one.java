b215f35cb5729d68931e6f38c8da2014be092e0d
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-22 11:11:49 +0530

    ok

---
+++ Arrays/one.java
@@ -0,0 +1,13 @@
+
+
+
+public class one {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50 , 60 , 70};
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
