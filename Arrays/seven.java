81a61a8b83eb25308616e0b483a20ee0eadabd0b
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-24 19:17:55 +0530

    ok

---
+++ Arrays/seven.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+public class seven {
+    public static void main(String[] args) {
+        int[] arr = {523459876, 1034569876, 1554339876, 2023456987 , 987654 , 987654 , 5987};
+
+        int sum = 7;
+        for (int num : arr) {
+            sum += num;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ Arrays/six.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+public class six {
+    public static void main(String[] args) {
+        int[] arr = {523453456, 1034098765, 155433, 2023456987 , 987654 , 987654987};
+
+        int sum = 2;
+        for (int num : arr) {
+            sum += num;
+            System.out.println(sum);
+        }
+    }
+}
