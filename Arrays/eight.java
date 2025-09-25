9840fcb061e533219e603099af5bb95ab708e1ee
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-25 18:42:56 +0530

    ok

---
+++ Arrays/eight.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class eight {
+    public static void main(String[] args) {
+        int[] arr = {523987676, 1987654876, 13456786, 287654387 , 98769954 , 98765094 , 59987687 , 9876549 , 9876543, 876543 , 87654 };
+
+        int sum = 7;
+        for (int num : arr) {
+            sum += num;
+            System.out.println(sum);
+        }
+    }
+}
---
+++ Arrays/nine.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+public class nine {
+    public static void main(String[] args) {
+        int[] arr = {459876, 69876, 39876, 456987 , 987654 , 987654 , 5987};
+
+        int sum = 7;
+        for (int num : arr) {
+            sum += num;
+            System.out.println(sum);
+        }
+    }
+}
