f2696a3a53069c4add2aead0f51edd834775ea5a
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-09-26 18:45:36 +0530

    ok

---
+++ Arrays/ten.java
@@ -0,0 +1,17 @@
+
+
+
+public class ten {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1067, 207644, 3034445, 409988774, 50876556};
+        System.out.println("\nUsing for-each loop:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+
+        
+
+    }
+}
