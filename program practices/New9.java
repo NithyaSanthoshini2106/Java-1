b4301a659d8a17f5d9f1ca235f38b1850e1a0020
NithyaSanthoshini2106 <nithyasanthoshini099@gmail.com>
2025-10-01 16:14:54 +0530

    ok

---
+++ program practices/New9.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class New9 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 65, 76, 98};
+
+        System.out.println("Original Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nReversed Array:");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
